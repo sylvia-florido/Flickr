f6995b656b0ecc0c774c30d49c7b5a29aff4101d
sflorido <sflorido@gmail.com>
2024-06-18 17:16:22 -0300

    Initial Commit

---
+++ Flickr/FlickrApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickrApp.swift
+//  Flickr
+//
+//  Created by Silvia Florido on 18/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
